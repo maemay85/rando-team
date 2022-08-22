5dfc236860d9b5f50d6fa870a16a909cdfda9df9
maemay85 <110618766+maemay85@users.noreply.github.com>
2022-08-22 11:02:19 -0400

    created rando_team project and created models

--- teamsApp/models.py
+++ teamsApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    team_name = models.CharField(max_length=420)
+    #maybe a picture later on
+
+class Member(models.Model):
+    team = models.ForeignKey(Team, on_delete = models.CASCADE)
+    member_name = models.CharField(max_length = 420)
+#members default to being opted in
+    leads_events = models.BooleanField(default=True)
+    #maybe a picture later on
